d4d24e06817d0a2b561955c4403da8db34ebc26f
Raj Kumar Gupta <raj@Rajs-MacBook-Pro.local>
2020-03-02 16:01:49 +0530

    files uploaded

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import start from './start';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => start);
